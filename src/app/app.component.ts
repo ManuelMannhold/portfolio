0efd26305794a1a951b4efb0bf99dabc4eea4c7a
ManuelMannhold <M.Mannhold@gmx.de>
2024-10-11 11:47:14 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, HeaderComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, ContactComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'portfolio';
+}
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,55 @@
+import { style } from '@angular/animations';
+import { Component } from '@angular/core';
+import { DeferBlockFixture } from '@angular/core/testing';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.scss'
+})
+export class ContactComponent {
+
+  constructor() {
+    this.inputBorder();
+    this.contactMe();
+  }
+
+  // buttonEnabled() {
+  //   let button = document.getElementById('contact-message-send-button');
+  //   let buttonAble:boolean = true;
+
+  //   if(!buttonAble) {
+  //     button?.classList.add('button-disabled');
+  //   }
+  //   else {
+  //     button?.classList.add('button');
+  //     }
+  //   }
+
+  contactMe() {
+    document.getElementById('contact-me-focus')?.addEventListener('click', () => {
+      document.getElementById('input-name')?.focus();
+    })
+  }
+
+  inputBorder() {
+    let inputName = document.getElementById('input-name') as HTMLInputElement;
+    let inputMail = document.getElementById('input-mail') as HTMLInputElement;
+    let inputMessage = document.getElementById('input-message') as HTMLTextAreaElement;
+
+    if (inputName && inputMail && inputMessage) {
+        if (inputName.value === '' || inputMail.value === '' || inputMessage.value === '') {
+            inputName.style.border = '1px solid red';
+            inputMail.classList.add('border-red');
+            inputMessage.classList.add('border-red');
+        } else {
+            inputName.style.border = '1px solid green';
+            inputMail.classList.add('border-green');
+            inputMessage.classList.add('border-green');
+        }
+    }
+}
+
+}
