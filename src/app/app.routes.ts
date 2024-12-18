205e20d923bc119084337f4e0eba1b6fb594602a
ManuelMannhold <M.Mannhold@gmx.de>
2024-12-18 21:04:06 +0100

    add privacy-policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { ImprintComponent } from './imprint/imprint.component';
 import { MainContentComponent } from './main-content/main-content.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 
 export const routes: Routes = [
   { path: '', component: MainContentComponent },
-  { path: 'imprint', component: ImprintComponent }
+  { path: 'imprint', component: ImprintComponent },
+  {path: 'main-content', component: MainContentComponent},
+  {path: 'privacy-policy', component: PrivacyPolicyComponent}
 ];
