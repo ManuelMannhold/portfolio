77ff1ffe68f988229406c19cd841c7293be88237
ManuelMannhold <m.mannhold@gmx.de>
2024-12-20 21:45:41 +0100

    fix responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,9 @@ import { Routes } from '@angular/router';
 import { ImprintComponent } from './imprint/imprint.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
-import { LandingPageComponent } from './main-content/landing-page/landing-page.component';
-
 
 export const routes: Routes = [
-  { path: '', component: LandingPageComponent  },
+  { path: '', component: MainContentComponent },
   { path: 'imprint', component: ImprintComponent },
   { path: 'main-content', component: MainContentComponent },
   { path: 'privacy-policy', component: PrivacyPolicyComponent }
