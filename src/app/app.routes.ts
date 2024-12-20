d291e93bd7004b9969aa3b81cf69902430c04dc5
ManuelMannhold <m.mannhold@gmx.de>
2024-12-20 15:00:28 +0100

    add translate policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { ImprintComponent } from './imprint/imprint.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { LandingPageComponent } from './main-content/landing-page/landing-page.component';
 
 
 export const routes: Routes = [
-  { path: '', component: MainContentComponent },
+  { path: '', component: LandingPageComponent },
   { path: 'imprint', component: ImprintComponent },
   { path: 'main-content', component: MainContentComponent },
   { path: 'privacy-policy', component: PrivacyPolicyComponent }
