82a87b184dee28c5b5b0edb1f0b529c4d8a3c2dd
ManuelMannhold <M.Mannhold@gmx.de>
2026-01-16 11:14:50 +0100

    change structure and add angular projects

--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -1,13 +1,43 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { PortfolioService } from './portfolio.service';
+import { Project, ProjectType } from './project.model';
+
+type FilterType = 'all' | ProjectType;
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit {
   private translateService = inject(TranslateService);
+  private portfolioService = inject(PortfolioService);
+
+  activeFilter: FilterType = 'all';
+  filteredProjects: Project[] = [];
+  private allProjects: Project[] = [];
+
+  ngOnInit(): void {
+    this.allProjects = this.portfolioService.getProjects();
+    this.applyFilter();
+  }
+
+  setFilter(filter: FilterType): void {
+    this.activeFilter = filter;
+    this.applyFilter();
+  }
+
+  private applyFilter(): void {
+    if (this.activeFilter === 'all') {
+      this.filteredProjects = this.allProjects.slice();
+    } else {
+      this.filteredProjects = this.allProjects.filter(
+        (project) => project.type === this.activeFilter
+      );
+    }
+  }
 }
